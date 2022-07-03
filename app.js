31acd251f6c2b538a84b7151df252478c4ca5fb3
git\kodirovdev0 <dovudhonq533@gmail.com>
2022-07-03 01:16:06 -0700

    adasdasd

---
+++ app.js
@@ -0,0 +1,205 @@
+item1.addEventListener('mouseover', () =>{
+    left1.style = "left: -20px; filter: opacity(1)";
+    right1.style = "right: -20px; filter: opacity(1)";
+    left2.style = "left: 0px; filter: opacity(0)";
+    right2.style = "right: 0px; filter: opacity(0)";
+    left3.style = "left: 0px; filter: opacity(0)";
+    right3.style = "right: 0px; filter: opacity(0)";
+    left4.style = "left: 0px; filter: opacity(0)";
+    right4.style = "right: 0px; filter: opacity(0)";
+    left5.style = "left: 0px; filter: opacity(0)";
+    right5.style = "right: 0px; filter: opacity(0)";
+
+})
+item2.addEventListener('mouseover', () =>{
+    left1.style = "left: 0px; filter: opacity(0)";
+    right1.style = "right: 0px; filter: opacity(0)";
+    left2.style = "left: -20px; filter: opacity(1)";
+    right2.style = "right: -20px; filter: opacity(1)";
+    left3.style = "left: 0px; filter: opacity(0)";
+    right3.style = "right: 0px; filter: opacity(0)";
+    left4.style = "left: 0px; filter: opacity(0)";
+    right4.style = "right: 0px; filter: opacity(0)";
+    left5.style = "left: 0px; filter: opacity(0)";
+    right5.style = "right: 0px; filter: opacity(0)";
+})
+item3.addEventListener('mouseover', () =>{
+    left1.style = "left: 0px; filter: opacity(0)";
+    right1.style = "right: 0px; filter: opacity(0)";
+    left2.style = "left: 0px; filter: opacity(0)";
+    right2.style = "right: 0px; filter: opacity(0)";
+    left3.style = "left: -20px; filter: opacity(1)";
+    right3.style = "right: -20px; filter: opacity(1)";
+    left4.style = "left: 0px; filter: opacity(0)";
+    right4.style = "right: 0px; filter: opacity(0)";
+    left5.style = "left: 0px; filter: opacity(0)";
+    right5.style = "right: 0px; filter: opacity(0)";
+})
+item4.addEventListener('mouseover', () =>{
+    left1.style = "left: 0px; filter: opacity(0)";
+    right1.style = "right: 0px; filter: opacity(0)";
+    left2.style = "left: 0px; filter: opacity(0)";
+    right2.style = "right: 0px; filter: opacity(0)";
+    left3.style = "left: 0px; filter: opacity(0)";
+    right3.style = "right: 0px; filter: opacity(0)";
+    left4.style = "left: -20px; filter: opacity(1)";
+    right4.style = "right: -20px; filter: opacity(1)";
+    left5.style = "left: 0px; filter: opacity(0)";
+    right5.style = "right: 0px; filter: opacity(0)";
+})
+item5.addEventListener('mouseover', () =>{
+    left1.style = "left: 0px; filter: opacity(0)";
+    right1.style = "right: 0px; filter: opacity(0)";
+    left2.style = "left: 0px; filter: opacity(0)";
+    right2.style = "right: 0px; filter: opacity(0)";
+    left3.style = "left: 0px; filter: opacity(0)";
+    right3.style = "right: 0px; filter: opacity(0)";
+    left4.style = "left: 0px; filter: opacity(0)";
+    right4.style = "right: 0px; filter: opacity(0)";
+    left5.style = "left: -20px; filter: opacity(1)";
+    right5.style = "right: -20px; filter: opacity(1)";
+})
+
+item01.addEventListener('mouseover', () =>{
+    left01.style = "left: -20px; filter: opacity(1)";
+    right01.style = "right: -20px; filter: opacity(1)";
+    left02.style = "left: 0px; filter: opacity(0)";
+    right02.style = "right: 0px; filter: opacity(0)";
+    left03.style = "left: 0px; filter: opacity(0)";
+    right03.style = "right: 0px; filter: opacity(0)";
+})
+item02.addEventListener('mouseover', () =>{
+    left01.style = "left: 0px; filter: opacity(0)";
+    right01.style = "right: 0px; filter: opacity(0)";
+    left02.style = "left: -20px; filter: opacity(1)";
+    right02.style = "right: -20px; filter: opacity(1)";
+    left03.style = "left: 0px; filter: opacity(0)";
+    right03.style = "right: 0px; filter: opacity(0)";
+})
+item03.addEventListener('mouseover', () =>{
+    left01.style = "left: 0px; filter: opacity(0)";
+    right01.style = "right: 0px; filter: opacity(0)";
+    left02.style = "left: 0px; filter: opacity(0)";
+    right02.style = "right: 0px; filter: opacity(0)";
+    left03.style = "left: -20px; filter: opacity(1)";
+    right03.style = "right: -20px; filter: opacity(1)";
+})
+item11.addEventListener('mouseover', () =>{
+    left11.style = "left: -20px; filter: opacity(1)";
+    right11.style = "right: -20px; filter: opacity(1)";
+    left12.style = "left: 0px; filter: opacity(0)";
+    right12.style = "right: 0px; filter: opacity(0)";
+    left13.style = "left: 0px; filter: opacity(0)";
+    right13.style = "right: 0px; filter: opacity(0)";
+    left14.style = "left: 0px; filter: opacity(0)";
+    right14.style = "right: 0px; filter: opacity(0)";
+    left15.style = "left: 0px; filter: opacity(0)";
+    right15.style = "right: 0px; filter: opacity(0)";
+
+})
+item12.addEventListener('mouseover', () =>{
+    left11.style = "left: 0px; filter: opacity(0)";
+    right11.style = "right: 0px; filter: opacity(0)";
+    left12.style = "left: -20px; filter: opacity(1)";
+    right12.style = "right: -20px; filter: opacity(1)";
+    left13.style = "left: 0px; filter: opacity(0)";
+    right13.style = "right: 0px; filter: opacity(0)";
+    left14.style = "left: 0px; filter: opacity(0)";
+    right14.style = "right: 0px; filter: opacity(0)";
+    left15.style = "left: 0px; filter: opacity(0)";
+    right15.style = "right: 0px; filter: opacity(0)";
+})
+item13.addEventListener('mouseover', () =>{
+    left11.style = "left: 0px; filter: opacity(0)";
+    right11.style = "right: 0px; filter: opacity(0)";
+    left12.style = "left: 0px; filter: opacity(0)";
+    right12.style = "right: 0px; filter: opacity(0)";
+    left13.style = "left: -20px; filter: opacity(1)";
+    right13.style = "right: -20px; filter: opacity(1)";
+    left14.style = "left: 0px; filter: opacity(0)";
+    right14.style = "right: 0px; filter: opacity(0)";
+    left15.style = "left: 0px; filter: opacity(0)";
+    right15.style = "right: 0px; filter: opacity(0)";
+})
+item14.addEventListener('mouseover', () =>{
+    left11.style = "left: 0px; filter: opacity(0)";
+    right11.style = "right: 0px; filter: opacity(0)";
+    left12.style = "left: 0px; filter: opacity(0)";
+    right12.style = "right: 0px; filter: opacity(0)";
+    left13.style = "left: 0px; filter: opacity(0)";
+    right13.style = "right: 0px; filter: opacity(0)";
+    left14.style = "left: -20px; filter: opacity(1)";
+    right14.style = "right: -20px; filter: opacity(1)";
+    left15.style = "left: 0px; filter: opacity(0)";
+    right15.style = "right: 0px; filter: opacity(0)";
+})
+item15.addEventListener('mouseover', () =>{
+    left11.style = "left: 0px; filter: opacity(0)";
+    right11.style = "right: 0px; filter: opacity(0)";
+    left12.style = "left: 0px; filter: opacity(0)";
+    right12.style = "right: 0px; filter: opacity(0)";
+    left13.style = "left: 0px; filter: opacity(0)";
+    right13.style = "right: 0px; filter: opacity(0)";
+    left14.style = "left: 0px; filter: opacity(0)";
+    right14.style = "right: 0px; filter: opacity(0)";
+    left15.style = "left: -20px; filter: opacity(1)";
+    right15.style = "right: -20px; filter: opacity(1)";
+})
+
+
+const login = document.querySelector(".login_btn") 
+const popup = document.querySelector(".popup") 
+const main = document.querySelector(".none") 
+const name__value = document.querySelector(".name") 
+const pass__value = document.querySelector(".pass") 
+const btn = document.querySelector(".signBtn") 
+const change = document.querySelector(".change") 
+const change_txt = document.querySelector(".change_text") 
+const SignUp2 = document.querySelector(".signBtn") 
+ 
+let nameValue = name__value.value 
+let passValue = pass__value.value 
+ 
+login.addEventListener("click", ()=> { 
+    popup.style.display = "block" 
+    popup.style.transform = "translateY(50px)" 
+    main.style.display = "none" 
+    document.body.style.height = "auto" 
+    btn.addEventListener("click", ()=> { 
+        localStorage.setItem("name", name__value.value) 
+        localStorage.setItem("password", pass__value.value) 
+        popup.style.display = "none" 
+        popup.style.transform = "translateY(-600px)" 
+        main.style.display = "block" 
+        change_txt.innerHTML = name__value.value 
+        change.innerHTML = "SIGN OUT"
+        SignUp2.addEventListener("click", ()=> {
+            main.style.background = "transparent"
+        })
+        change.addEventListener("click", ()=> { 
+            localStorage.removeItem("name", name__value.value) 
+            localStorage.removeItem("password", pass__value.value) 
+            change.innerHTML = "LOGIN" 
+            change_txt.style.display = "none"
+            main.style.display = "none"
+            document.body.style.background = "black"
+           
+        }) 
+    }) 
+})
+
+
+
+
+// item.forEach(it => {
+//     it.addEventListener("click", () => {
+//         item.forEach(it2 => {
+//             it2.style.color = "black";
+//             it2.style.border = "1px solid gray";
+//         })
+//         it.style.color = "rgb(0, 121, 143)";
+//         it.style.border = "2px solid rgb(0, 121, 143)";
+//     })
+
+// })
+
